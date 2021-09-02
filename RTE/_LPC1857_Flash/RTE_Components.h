13b968fead307390427cd5a80d97d40041dab8f6
hcosgun1 <53692244+hcosgun1@users.noreply.github.com>
2021-09-02 11:37:01 -0400

    first commit

--- RTE/_LPC1857_Flash/RTE_Components.h
+++ RTE/_LPC1857_Flash/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Component Configuration File
+ * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'Blinky' 
@@ -10,8 +10,17 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LPC18xx.h"
+
+/* ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
 #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
         #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/* Keil::Device:Startup:1.0.0 */
 #define RTE_DEVICE_STARTUP_LPC18XX      /* Device Startup for NXP18XX */
+
 
 #endif /* RTE_COMPONENTS_H */
